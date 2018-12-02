4a28f452bc9754bb879c79477e85142d3196b6f3
Yoann Lehmann <yoann.lehmann@he-arc.ch>
2018-12-02 17:58:43 -0500

    Ajout des informations du match dans la page de détails.

--- src/public/pages/suivimatch/suivimatch.js
+++ src/public/pages/suivimatch/suivimatch.js
@@ -5,4 +5,45 @@ angular.module('Betennis').controller('suiviMatchCtrl', ['$scope', '_', "$stateP
     if($scope.match===null) {
         $scope.match = angular.fromJson(localStorage.getItem("liste-match"))[$stateParams.id];
     }
+    console.log($scope.match);
+
+    $scope.convertScore = function (scoreFromZeroToThree) {
+        switch (scoreFromZeroToThree) {
+            case 1:
+                return 15;
+            case 2:
+                return 30;
+            case 3:
+                return 40;
+            default:
+                return 0;
+        }
+    };
+
+    $scope.convertTime = function (timeInSeconds) {
+        let nbHours = Math.floor(timeInSeconds / 3600);
+        let nbMinutes = timeInSeconds % 60;
+
+        if (nbHours < 10) {
+            nbHours = "0" + nbHours;
+        }
+
+        if (nbMinutes < 10) {
+            nbMinutes = "0" + nbMinutes;
+        }
+
+        return nbHours + "h" + nbMinutes;
+    };
+
+    $scope.displayResult = function(nbOfSet)
+    {
+        if(nbOfSet >= 2)
+        {
+            return "win";
+        }
+        else
+        {
+            return "lose";
+        }
+    }
 }]);
