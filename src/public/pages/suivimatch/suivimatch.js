230c267ac6fa3887e3cf508269b2147ec55dd76a
Yoann Lehmann <yoann.lehmann@he-arc.ch>
2018-12-02 21:21:17 -0500

    Affichage du modal pour les paris

--- src/public/pages/suivimatch/suivimatch.js
+++ src/public/pages/suivimatch/suivimatch.js
@@ -1,12 +1,42 @@
 'use strict';
 
 angular.module('Betennis').controller('suiviMatchCtrl', ['$scope', '_', "$stateParams", "$uibModal",
+
     function ($scope, _, $stateParams, $modalInstance) {
     $scope.match = angular.fromJson($stateParams.match);
     if($scope.match===null) {
         $scope.match = angular.fromJson(localStorage.getItem("liste-match"))[$stateParams.id];
     }
     console.log($scope.match);
+    $scope.instance = null;
+
+
+    $scope.openModal = function (size, parentSelector, playerToBet) {
+
+        var parentElem = parentSelector ?
+            angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+        var modalInstance = $modalInstance.open({
+            animation: this.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'modalparis.html',
+            controller: 'suiviMatchCtrl',
+            controllerAs: '$ctrl',
+            size: size,
+            appendTo: parentElem,
+            resolve: {
+                items: function () {
+                    return null;//this.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            this.selected = selectedItem;
+        }, function () {
+            //$log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 
     $scope.convertScore = function (scoreFromZeroToThree) {
         switch (scoreFromZeroToThree) {
@@ -46,5 +76,35 @@ angular.module('Betennis').controller('suiviMatchCtrl', ['$scope', '_', "$stateP
         {
             return "lose";
         }
+    }
+
+    $scope.openBetModal = function()
+    {
+
+            $scope.instance = $modalInstance.open({
+                animation: this.animationsEnabled,
+                ariaLabelledBy: 'modal-title-top',
+                ariaDescribedBy: 'modal-body-top',
+                controller: 'suiviMatchCtrl',
+                size: 'sm',
+                templateUrl: 'pages/suivimatch/modalparis.html'
+            });
+
+            $scope.instance.result.then(function () {
+
+            }, function () {
+
+            });
+
+    }
+
+    $scope.annulerParis = function(result)
+    {
+        $scope.instance.close(result);
+    }
+
+    $scope.validerParis = function()
+    {
+        $scope.instance.close();
     }
 }]);
